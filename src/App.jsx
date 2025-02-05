593f656f1d93fafe90ebbeb0bfff48b469241eb4
Buhari <sybuhari234@gmail.com>
2025-02-05 18:25:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './Home'
+import About from './About'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+    
+    <Navbar />
+    <Routes>
+      <Route path='/home' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { FaBars } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div>
-      Nav
+    <div className="nav">
+      <Link to={'/home'}>Home</Link>
+      <Link to={'/about'}>About Us</Link>
+      <button><FaBars /></button>
     </div>
   )
 }
